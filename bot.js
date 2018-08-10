2481d319602b2b3b95308a737c8f171409dffa7d
wskyhrb <41772742+wskyhrb@users.noreply.github.com>
2018-08-10 08:42:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,6 +22,36 @@ const client = new Discord.Client();
 });
  
  
- 
+ const misaka = new Set();
+var prefix = '*';
+    client.on('message', async msg => {
+  if(msg.content.startsWith("رابط")) {
+  if (misaka.has(msg.author.id)) {
+    let misakaemb = new Discord.RichEmbed()
+    .setDescription(`يجب عليك الانتظار 24 ساعه!`)
+    .setColor(`RED`)
+    return msg.channel.send(misakaemb).then(message => {
+     message.delete(10000) 
+    })
+    
+    }
+    misaka.add(msg.author.id);
+
+  
+   msg.channel.createInvite({
+        thing: true,
+        maxUses: 5,
+        maxAge: 86400,
+  }).then(invite =>
+   msg.author.sendMessage(`
+         <@${msg.author.id}> 
+         **maxUses: 5 **
+         ${invite.url}`)
+  )
+    msg.channel.send(`**:link: Invite Linke Sent In DM Successfully**`)
+  }
+    setTimeout(() => {
+    },86400000);
+    })
  
  client.login(process.env.BOT_TOKEN);
