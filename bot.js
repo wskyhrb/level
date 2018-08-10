364346307a3182ed17a5baa030c47e3a3bbc4673
wskyhrb <41772742+wskyhrb@users.noreply.github.com>
2018-08-10 17:30:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -54,4 +54,15 @@ var prefix = '*';
     },86400000);
     })
  
+
+
+client.on('message', message => {
+    if (message.content.startsWith("دعواتي")) {
+
+    message.guild.fetchInvites()
+    .then(invites => message.channel.send(**:busts_in_silhouette:  اتيت ب     [${invites.find(invite => invite.inviter.id === message.author.id)}]    :calling:   عضو لهذا السيرفر))
+
+    }
+});
+
  client.login(process.env.BOT_TOKEN);
