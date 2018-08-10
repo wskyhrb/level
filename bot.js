a247d7bf5af67b0005f2239dc7253310a5df41d2
wskyhrb <41772742+wskyhrb@users.noreply.github.com>
2018-08-10 08:17:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,27 @@
 
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+
+
+
+
+
+
+
+
+ client.on('message', message => {
+    var prefix = "b-"
+    if (message.content === prefix + "date") {
+        var currentTime = new Date(),
+            السنة = currentTime.getFullYear(),
+            الشهر = currentTime.getMonth() + 1,
+            اليوم = currentTime.getDate();
+        message.channel.sendMessage( "التاريخ : " + اليوم + "-" + الشهر + "-" +السنة)
+    }
+});
+ 
+ 
+ 
+ 
+ client.login(process.env.BOT_TOKEN);
