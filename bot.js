3734c8f4527a8cec8ac5913f71300927b289e65e
wskyhrb <41772742+wskyhrb@users.noreply.github.com>
2018-08-13 08:27:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -86,4 +86,27 @@ client.on('guildMemberAdd', member => {
       channel.sendEmbed(embed);
     });
 
+client.on("message", message => {
+    var prefix = "+";
+ 
+            var args = message.content.substring(prefix.length).split(" ");
+            if (message.content.startsWith(prefix + "مسح")) {
+   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('⚠ | **لا يوجد لديك صلاحية لمسح الشات**');
+        var msg;
+        msg = parseInt();
+      
+      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+      message.channel.sendMessage("", {embed: {
+        title: "Done | تــم مسح الشات",
+        color: 0x06DF00,
+        description: "تم مسح الرسائل ",
+        footer: {
+          text: "©KBOOSHDev"
+        }
+      }}).then(msg => {msg.delete(3000)});
+                          }
+
+     
+});
+
  client.login(process.env.BOT_TOKEN);
