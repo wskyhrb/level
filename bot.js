cd60fbda9f856e4161552c4816022d6e1927e0a9
wskyhrb <41772742+wskyhrb@users.noreply.github.com>
2018-12-13 19:22:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,8 @@ const developers = ['286926707531841536'];
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setGame(`. FủLiĨ "#1813`,'https://www.twitch.tv/MeeRcY')
+  client.user.setGame(`.تصادق مع الذئاب .. على أن يكون فأسك مستعداً.
+`,'https://www.twitch.tv/MeeRcY')
   client.user.setStatus("idle")
 }); 
 client.login(process.env.BOT_TOKEN);
