118f9fc7d86d26c7d89ac0c9c0a43e1ee4dc01f3
wskyhrb <41772742+wskyhrb@users.noreply.github.com>
2018-12-15 15:54:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,22 +5,5 @@ const developers = ['286926707531841536'];
 
 
 
-client.on('message', message => {
-  var argresult = message.content.split(` `).slice(1).join(' ');
-    if (!developers.includes(message.author.id)) return;
-
-if (message.content.startsWith('!w')) {
-client.user.setActivity(argresult, {type:'WATCHING'});
-    message.channel.send(`${argresult} \` `)
-} else 
-if (message.content.startsWith('!l')) {
-client.user.setActivity(argresult , {type:'LISTENING'});
-    message.channel.send(` ${argresult} \` `)
-} else 
-if (message.content.startsWith('!s')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/meercy");
-   message.channel.send(`${argresult} \` ` )
-}
-});
-
+\
 client.login(process.env.BOT_TOKEN);
