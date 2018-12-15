310b554fe9c4c626dd11a113966c0a941441b546
wskyhrb <41772742+wskyhrb@users.noreply.github.com>
2018-12-15 03:32:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,18 +3,24 @@ const client = new Discord.Client();
 const developers = ['286926707531841536'];
 
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setGame(`.تصادق مع الذئاب .. على أن يكون فأسك مستعداً.
-`,'https://www.twitch.tv/MeeRcY')
-  client.user.setStatus("idle")
-}); 
 
 
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!developers.includes(message.author.id)) return;
 
-
-
-
-
+if (message.content.startsWith('!w')) {
+client.user.setActivity(argresult, {type:'WATCHING'});
+    message.channel.send(`${argresult} \` `)
+} else 
+if (message.content.startsWith('!l')) {
+client.user.setActivity(argresult , {type:'LISTENING'});
+    message.channel.send(` ${argresult} \` `)
+} else 
+if (message.content.startsWith('!s')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/meercy");
+   message.channel.send(`${argresult} \` `)
+}
+});
 
 client.login(process.env.BOT_TOKEN);
